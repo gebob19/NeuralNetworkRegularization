8c0533554d6ca0297f27e739e08a56a7e7fff4e1
Brennan Gebotys <gebob19.git@gmail.com>
2020-11-16 09:02:31 -0500

    update tf records for jpeg compression

--- organize_data.py
+++ organize_data.py
@@ -163,15 +163,12 @@ for dset_file, data in zip(['train.txt', 'test.txt', 'val.txt'], [train, test, v
             line = '{} {}'.format(fn, label)
             f.write(line)
 
+###### TF RECORDS ROUGH WORK ####### 
+
 # %%
 with open(DATAFILE_PATH+'train.txt', 'r') as f: 
     lines = f.readlines()
     
-# line = lines[0]
-
-# #%%
-# d = line.split(' ')
-# fn, label = '{}{}'.format(PATH2VIDEOS, d[0]), int(d[1])
 def mp4_2_numpy(filename):
     vid = imageio.get_reader(filename, 'ffmpeg')
     data = np.stack(list(vid.iter_data()))
@@ -183,34 +180,58 @@ def _int64_feature(value):
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+N_FRAMES = 10 
+
 def line2example(line):
     d = line.split(' ')
     fn, label = '{}{}'.format(PATH2VIDEOS, d[0]), int(d[1])
+    
+    # read file for shape 
     data = mp4_2_numpy(fn)
-
-    feature = {}
     t, h, w, c = data.shape
-    feature['filename'] = _bytes_feature(fn.encode('utf-8'))
-    feature['data'] = _bytes_feature(data.tostring())
-    feature['label'] = _int64_feature(label)
-    feature['temporal'] = _int64_feature(t)
-    feature['height'] = _int64_feature(h)
-    feature['width'] = _int64_feature(w)
-    feature['depth'] = _int64_feature(c)
+    # sample 10 random frames  -- fps == 24 
+    sampled_frame_idxs = np.linspace(0, t-1, num=N_FRAMES, dtype=np.int32)
+    vid = vid[sampled_frame_idxs]
 
-    example = tf.train.Example(features = tf.train.Features(feature=feature))
+    img_bytes = [tf.image.encode_jpeg(d, format='rgb') for d in data]
+
+    with tf.Session() as sess: 
+        img_bytes = sess.run(img_bytes)
+    
+    img_feats = [tf.train.Feature(bytes_list=tf.train.BytesList(value=[imgb])) for imgb in img_bytes]
+
+    sequence_dict = {}
+    sequence_dict['data'] = tf.train.FeatureList(feature=img_feats)
+
+    context_dict = {}
+    context_dict['filename'] = _bytes_feature(fn.encode('utf-8'))
+    context_dict['label'] = _int64_feature(label)
+    context_dict['temporal'] = _int64_feature(t)
+    context_dict['height'] = _int64_feature(h)
+    context_dict['width'] = _int64_feature(w)
+    context_dict['depth'] = _int64_feature(c)
+
+    sequence_context = tf.train.Features(feature=context_dict)
+    sequence_list = tf.train.FeatureLists(feature_list=sequence_dict)
+
+    example = tf.train.SequenceExample(context=sequence_context, feature_lists=sequence_list)
 
     return example
 
-with tf.python_io.TFRecordWriter('example.tfrecord') as writer: 
-    for line in lines[:8]:
+with tf.python_io.TFRecordWriter('data/example2.tfrecord') as writer: 
+    for line in tqdm(lines[:4]):
         example = line2example(line)
         writer.write(example.SerializeToString())
 
 #%%
-feature_description = {
+tf.reset_default_graph()
+
+sequence_features = {
+    'data': tf.FixedLenSequenceFeature([], dtype=tf.string)
+}
+
+context_features = {
     'filename': tf.io.FixedLenFeature([], tf.string),
-    'data': tf.io.FixedLenFeature([], tf.string),
     'height': tf.io.FixedLenFeature([], tf.int64),
     'width': tf.io.FixedLenFeature([], tf.int64),
     'depth': tf.io.FixedLenFeature([], tf.int64),
@@ -220,31 +241,46 @@ feature_description = {
 
 @tf.function
 def resize(x):
-    return tf.image.resize(x, [IMAGE_SIZE_H, IMAGE_SIZE_W], antialias=True)
+    return tf.image.resize(x, [IMAGE_SIZE_H, IMAGE_SIZE_W])
 
 def _parse_image_function(example_proto):
     # Parse the input tf.train.Example proto using the dictionary above.
-    metadata = tf.io.parse_single_example(example_proto, feature_description)
+    context, sequence = tf.parse_single_sequence_example(
+            example_proto, context_features=context_features, sequence_features=sequence_features)
     
-    shape = (metadata['temporal'], metadata['height'], metadata['width'], metadata['depth'])
-    video3D_buffer = tf.reshape(metadata['data'], shape=[])
-    video3D = tf.decode_raw(video3D_buffer, tf.uint8)
+    shape = (context['temporal'], context['height'], context['width'], context['depth'])
+
+    # literally had to brute force this shit to get it working 
+    video_data = tf.expand_dims(
+        tf.image.decode_image(tf.gather(sequence['data'], [0])[0]), 0)
+    i = tf.constant(1, dtype=tf.int32)
+    cond = lambda i, _: tf.less(i, tf.cast(context['temporal'], tf.int32))
+    def body(i, video_data):
+        video3D = tf.gather(sequence['data'], [i])
+        img_data = tf.image.decode_image(video3D[0]) 
+        video_data = tf.concat([video_data, [img_data]], 0)
+        return (tf.add(i, 1), video_data)
+
+    _, video_data = tf.while_loop(cond, body, [i, video_data], 
+        shape_invariants=[i.get_shape(), tf.TensorShape([None])])
+    video3D = video_data
+    # use this to set the shape -- doesn't change anything 
     video3D = tf.reshape(video3D, shape)
 
     # sample across temporal frames 
     n_frames = 10 
     idxs = tf.random.uniform((n_frames,), minval=0,
-            maxval=tf.cast(metadata['temporal'], tf.int32), 
+            maxval=tf.cast(context['temporal'], tf.int32), 
             dtype=tf.int32)
     video3D = tf.cast(tf.gather(video3D, idxs), tf.float32)
 
     video3D = tf.map_fn(resize, video3D, back_prop=False, parallel_iterations=10)
 
-    label = metadata['label']
+    label = context['label']
 
-    return video3D, label, metadata['filename']
+    return video3D, label, context['filename'], shape
 
-dataset = tf.data.TFRecordDataset('example.tfrecord')\
+dataset = tf.data.TFRecordDataset('data/example2.tfrecord')\
     .map(_parse_image_function)\
     .batch(3)
 
@@ -255,7 +291,8 @@ next_element = iterator.get_next()
 with tf.Session() as sess: 
     sess.run([tf.compat.v1.global_variables_initializer(), \
             tf.compat.v1.local_variables_initializer()])
-    while True: 
-        e = sess.run(next_element)
-        print(e)
+    for _ in range(1):
+        vid, _, _, shape = sess.run(next_element)
+        print(vid.shape)
+
 # %%
--- tf_records.py
+++ tf_records.py
@@ -3,16 +3,19 @@ import tensorflow as tf
 import pathlib 
 import json
 import imageio 
-import numpy as np 
-import cv2 
+import numpy as np
+import cv2
+from tqdm import tqdm
 
 ## MAKE SURE TO SET THE FOLLOWING FROM CONFIG
-# - IMAGE DIMENSIONS 
-from config import * 
+# - IMAGE DIMENSIONS (default 224x224)
+from config import *
 
-## SET THIS **** 
+## SET THIS ****
+N_FRAMES = 10 
 TOP_N = 100
-VIP = True if N_GPUS > 1 else False 
+## sets the correct path to read datafiles from 
+VIP = True if N_GPUS > 1 else False
 DESKTOP = True if N_GPUS == 1 else False
 if VIP: 
     PATH2VIDEOS = '/home/brennan/672/videos/'
@@ -25,77 +28,79 @@ else:
     DATAFILE_PATH = '/Users/brennangebotys/Documents/gradschool/672/project/regularization_project/top-k-glosses/{}/'.format(TOP_N)
 
 # %%
-
 ##### video reading functions 
 def mp4_2_numpy(filename):
     vid = imageio.get_reader(filename, 'ffmpeg')
     data = np.stack(list(vid.iter_data()))
     return data 
 
-def fn_preprocess(fn):
-    vid = mp4_2_numpy(fn)
-    t, h, w, _ = vid.shape 
-
-    if h < IMAGE_SIZE_H or w < IMAGE_SIZE_W: 
-        vid = np.stack([cv2.resize(frame, (IMAGE_SIZE_W, IMAGE_SIZE_H)) for frame in vid])
-    else: 
-        # crop to IMAGE_SIZE x IMAGE_SIZE
-        h_start = (h - IMAGE_SIZE_H) // 2
-        w_start = (w - IMAGE_SIZE_W) // 2
-        vid = vid[:, h_start: h_start + IMAGE_SIZE_H, w_start: w_start + IMAGE_SIZE_W, :]
-    return vid
-
 ###### tf records functions 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def line2example(line):
-    """Takes a dataset line to a mp4 file and returns a tfrecords example.
-
-    Args:
-        line ([str]): in the format "{filename} {label}"
-    """
     d = line.split(' ')
     fn, label = '{}{}'.format(PATH2VIDEOS, d[0]), int(d[1])
-
-    ## dont pre-process (can fix this later if its better)
+    
+    # read file for shape 
     data = mp4_2_numpy(fn)
-
     t, h, w, c = data.shape
-    feature = {
-        'filename': _bytes_feature(fn.encode('utf-8')),
-        'data': _bytes_feature(data.tostring()),
-        'label': _int64_feature(label),
-        'temporal': _int64_feature(t),
-        'height': _int64_feature(h),
-        'width': _int64_feature(w),
-        'depth': _int64_feature(c),
-    }
+    # sample 10 random frames  -- fps == 24 
+    sampled_frame_idxs = np.linspace(0, t-1, num=N_FRAMES, dtype=np.int32)
+    data = data[sampled_frame_idxs]
 
-    example = tf.train.Example(features=tf.train.Features(feature=feature))
+    # save video as list of encoded frames 
+    img_bytes = [tf.image.encode_jpeg(d, format='rgb') for d in data]
+
+    with tf.Session() as sess: 
+        img_bytes = sess.run(img_bytes)
+    
+    img_feats = [tf.train.Feature(bytes_list=tf.train.BytesList(value=[imgb])) for imgb in img_bytes]
+
+    sequence_dict = {}
+    sequence_dict['data'] = tf.train.FeatureList(feature=img_feats)
+
+    context_dict = {}
+    context_dict['filename'] = _bytes_feature(fn.encode('utf-8'))
+    context_dict['label'] = _int64_feature(label)
+    context_dict['temporal'] = _int64_feature(t)
+    context_dict['height'] = _int64_feature(h)
+    context_dict['width'] = _int64_feature(w)
+    context_dict['depth'] = _int64_feature(c)
+
+    sequence_context = tf.train.Features(feature=context_dict)
+    sequence_list = tf.train.FeatureLists(feature_list=sequence_dict)
+
+    example = tf.train.SequenceExample(context=sequence_context, feature_lists=sequence_list)
+
     return example
 
 #%%    
-RECORDS_SAVE_PATH = pathlib.Path('data/top-{}/'.format(top-k-glosses))
+RECORDS_SAVE_PATH = pathlib.Path('data/top-{}/'.format(TOP_N))
 RECORDS_SAVE_PATH.mkdir(exist_ok=True, parents=True)
 
 for dset_name in ['train.txt', 'test.txt', 'val.txt']:
-    with open(DATAFILE_PATH + dset_name, 'r') as f: 
+    with open(DATAFILE_PATH + dset_name, 'r') as f:
         lines = f.readlines()
 
-    with tf.python_io.TFRecordWriter(RECORDS_SAVE_PATH/'{}.tfrecord'.format(dset_name[:-4])) as writer: 
-        for line in lines: 
+    record_file = str(RECORDS_SAVE_PATH/'{}.tfrecord'.format(dset_name[:-4]))
+    with tf.python_io.TFRecordWriter(record_file) as writer: 
+        for line in tqdm(lines[:2]): 
             example = line2example(line)
             writer.write(example.SerializeToString())
 
 #%%
 ################ READING THE DATA EXAMPLE ####################
 
-feature_description = {
+sequence_features = {
+    'data': tf.FixedLenSequenceFeature([], dtype=tf.string)
+}
+
+context_features = {
     'filename': tf.io.FixedLenFeature([], tf.string),
-    'data': tf.io.FixedLenFeature([], tf.string),
     'height': tf.io.FixedLenFeature([], tf.int64),
     'width': tf.io.FixedLenFeature([], tf.int64),
     'depth': tf.io.FixedLenFeature([], tf.int64),
@@ -109,36 +114,55 @@ def resize(x):
 
 def _parse_image_function(example_proto):
     # Parse the input tf.train.Example proto using the dictionary above.
-    metadata = tf.io.parse_single_example(example_proto, feature_description)
+    context, sequence = tf.parse_single_sequence_example(
+            example_proto, context_features=context_features, sequence_features=sequence_features)
     
-    shape = (metadata['temporal'], metadata['height'], metadata['width'], metadata['depth'])
-    video3D_buffer = tf.reshape(metadata['data'], shape=[])
-    video3D = tf.decode_raw(video3D_buffer, tf.uint8)
+    shape = (context['temporal'], context['height'], context['width'], context['depth'])
+
+    # literally had to brute force this shit to get it working 
+    video_data = tf.expand_dims(
+        tf.image.decode_image(tf.gather(sequence['data'], [0])[0]), 0)
+    i = tf.constant(1, dtype=tf.int32)
+    cond = lambda i, _: tf.less(i, tf.cast(context['temporal'], tf.int32))
+    def body(i, video_data):
+        video3D = tf.gather(sequence['data'], [i])
+        img_data = tf.image.decode_image(video3D[0]) 
+        video_data = tf.concat([video_data, [img_data]], 0)
+        return (tf.add(i, 1), video_data)
+
+    _, video_data = tf.while_loop(cond, body, [i, video_data], 
+        shape_invariants=[i.get_shape(), tf.TensorShape([None])])
+    video3D = video_data
+    # use this to set the shape -- doesn't change anything 
     video3D = tf.reshape(video3D, shape)
 
-    # sample across temporal frames 
-    n_frames = 10 
-    idxs = tf.random.uniform(
-            (n_frames,),
-            minval=0,
-            maxval=tf.cast(metadata['temporal'], tf.int32)-1,
-            dtype=tf.int32)
-    video3D = tf.cast(tf.gather(video3D, idxs), tf.float32)
+    # # sample across temporal frames 
+    # idxs = tf.random.uniform((N_FRAMES,), minval=0,
+    #         maxval=tf.cast(context['temporal'], tf.int32), 
+    #         dtype=tf.int32)
+    # video3D = tf.cast(tf.gather(video3D, idxs), tf.float32)
 
+    # resize images in video
     video3D = tf.map_fn(resize, video3D, back_prop=False, parallel_iterations=10)
 
-    label = metadata['label']
+    label = context['label']
 
-    return video3D, label, metadata['filename']
+    return video3D, label, context['filename'], shape
 
-#%%
-dataset = tf.data.TFRecordDataset('train.tfrecord')\
+dataset = tf.data.TFRecordDataset('data/example2.tfrecord')\
     .map(_parse_image_function)\
-    .batch(3)
+    .batch(2)
+
 iterator = dataset.make_one_shot_iterator()
 next_element = iterator.get_next()
 
+# %%
 with tf.Session() as sess: 
-    while True: 
-        e = sess.run(next_element)
-        print(e)
+    sess.run([tf.compat.v1.global_variables_initializer(), \
+            tf.compat.v1.local_variables_initializer()])
+    for _ in range(1):
+        vid, _, _, shape = sess.run(next_element)
+        print(vid.shape)
+
+
+# %%

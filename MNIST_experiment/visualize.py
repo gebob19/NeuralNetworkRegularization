dbbcddaf2f79d8b446e1674832a0143a0438a113
Brennan Gebotys <gebob19.git@gmail.com>
2020-11-23 13:05:49 -0500

    add cifar data

--- MNIST_experiment/visualize.py
+++ MNIST_experiment/visualize.py
@@ -1,6 +1,6 @@
 #%%
 import neptune
-project = neptune.init('gebob19/672-mnist')
+project = neptune.init('gebob19/672-cifar')
 
 # %%
 exps = project.get_experiments()
@@ -9,37 +9,43 @@ best_exps = [e for e in exps if 'best' in e.get_tags()]
 # %%
 def get_values(experiment):
     channel_names = list(experiment.get_logs().keys())
-    data = experiment.get_numeric_channels_values(*channel_names[:-1])
+    data = experiment.get_numeric_channels_values(*channel_names)
     return data 
 
 #%%
 best_exps_values = [get_values(e) for e in best_exps]
 
-#%%
-for data, exp in zip(best_exps_values, best_exps):
-    name = exp.name
-    l = exp.get_parameters()['reg_constant']
-    if name == 'DropoutReg':
-        l = exp.get_parameters()['dropout_constant']
-    if name == 'Baseline':
-        l = 'n/a'
-    test_acc = data['test_acc'].values[-1]
-    train_acc = data['train_acc'].values[-2]
-    loss = data['loss'].values[-2]
-    largest_singular_value = data['sum_singular_value'].values[-2]
-    w_norm = data['w_norm'].values[-2]
-
-    print('{}: {:.2f} {:.2f} {} {:.2f} {:.2f}'.format(name, test_acc, train_acc, l, largest_singular_value, w_norm)) 
-
-#%%
+# #%%
 # # remove dropout for better visualizations 
+# exp_names = [e.name for e in best_exps]
 # dropout_index = exp_names.index('DropoutReg')
 # del best_exps[dropout_index]
 # del best_exps_values[dropout_index]
 
 #%%
+for data, exp in zip(best_exps_values, best_exps):
+    name = exp.name
+    params = exp.get_parameters()
+    l = params['reg_constant']
+    if name == 'DropoutReg':
+        l = params['dropout_constant']
+    if name == 'Baseline':
+        l = 'n/a'
+    test_acc = data['test_acc'].values[-1]
+    train_acc = data['train_acc'].values[-2]
+    kreg, dreg = params['kernel_regularization'], params['dense_regularization']
+
+    # loss = data['loss'].values[-2]
+    # largest_singular_value = data['sum_singular_value'].values[-2]
+    # w_norm = data['w_norm'].values[-2]
+
+    # print('{}: {:.2f} {:.2f} {} {:.2f} {:.2f}'.format(name, test_acc, train_acc, l, largest_singular_value, w_norm)) 
+
+    print('{}: {:.3f} {:.3f} {} {}'.format(name, test_acc, train_acc, kreg, dreg)) 
+
+#%%
 import pathlib 
-chart_dir = pathlib.Path()/'charts/'
+chart_dir = pathlib.Path()/'charts-cifar/'
 chart_dir.mkdir(exist_ok=True, parents=True)
 
 # %%

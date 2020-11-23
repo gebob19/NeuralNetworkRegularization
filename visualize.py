0ba58bbe300d91c1414f30e8390813370ae856ed
Brennan Gebotys <gebob19.git@gmail.com>
2020-11-23 14:49:01 -0500

    add new chart

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 #%%
 import neptune
+import matplotlib.pyplot as plt 
 project = neptune.init('gebob19/672-cifar')
 
 # %%
@@ -49,8 +50,6 @@ chart_dir = pathlib.Path()/'charts/charts-cifar/'
 chart_dir.mkdir(exist_ok=True, parents=True)
 
 # %%
-import matplotlib.pyplot as plt 
-
 for col in best_exps_values[0].columns[1:]:
     for data, exp in zip(best_exps_values, best_exps): 
         y = data[col]
@@ -61,5 +60,23 @@ for col in best_exps_values[0].columns[1:]:
     plt.title('{} vs Epoch'.format(col))
     plt.savefig(chart_dir/'{}.png'.format(col))
     plt.clf()
+
+# %%
+plt.title('{} vs Epoch'.format('Accuracy'))
+
+from matplotlib.lines import Line2D
+line = Line2D([0,1],[0,1],linestyle='-', color='black')
+line2 = Line2D([0,1],[0,1],linestyle='--', color='black')
+colors = ['red', 'green', 'blue', 'orange', 'purple']
+for data, exp, c in zip(best_exps_values, best_exps, colors): 
+    plt.plot(data['train_acc'], color=c, label=exp.name)
+    plt.plot(data['val_acc'], '--', color=c)
+
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+l1 = plt.legend(loc=4)
+plt.legend([line, line2], ['train', 'val'], loc=2)
+plt.gca().add_artist(l1)
+plt.savefig(chart_dir/'accuracy-per-epoch.png')
 
 # %%
